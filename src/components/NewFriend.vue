e47e60a2bdf4f536c22a11bb783e43989d26ae48
Kosbeen <kosbeen44@gmail.com>
2021-11-17 10:32:07 +0300

    Добавлена возможность добавления нового друга

---
+++ src/components/NewFriend.vue
@@ -0,0 +1,41 @@
+<template>
+  <form @submit.prevent="addContact">
+    <div>
+      <label>Name</label>
+      <input type="name" v-model="enteredName" />
+    </div>
+    <div>
+      <label>Phone</label>
+      <input type="tel" v-model="enteredPhone" />
+    </div>
+    <div>
+      <label>Email</label>
+      <input type="email" v-model="enteredEmail" />
+    </div>
+    <div>
+      <button>Add contact</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      enteredName: "",
+      enteredPhone: "",
+      enteredEmail: "",
+    };
+  },
+  methods: {
+    addContact() {
+      this.$emit(
+        "add-friend",
+        this.enteredName,
+        this.enteredPhone,
+        this.enteredEmail
+      );
+    },
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import { createApp } from 'vue';
 import newApp from './App.vue'
 
 import FriendContacts from './components/FriendContacts.vue'
-
+import NewFriend from './components/NewFriend.vue'
 
 
 const app = createApp(newApp)
 app.component('friend-contact', FriendContacts)
+app.component('add-contact', NewFriend)
 app.mount('#app');
